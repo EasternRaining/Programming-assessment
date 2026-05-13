a046c71b3e00a234029bdaabcfa048f11b921bae
EasternRaining <dewantob4371@masseyhigh.school.nz>
2026-05-13 14:24:19 +1200

    Add files via upload

--- math quiz assessment.py
+++ math quiz assessment.py
@@ -16,6 +16,7 @@ def yes_no(question):
         else:
             print("Please enter yes / no")
 
+
 def instruction():
     print('''
 
@@ -117,7 +118,7 @@ while questions_played < num_questions:
     print(rounds_heading)
     print()
 
-    # Rounds start here
+    # Questions start here
     # set the guesses to 0 at the start of every round
     questions_answered = 0
     already_answered = []
@@ -128,5 +129,61 @@ while questions_played < num_questions:
     # check that they don't want to quit
     if answer == "xxx":
         # set end_game to use so that outer loop can be broken
-        end_game = "yes"
+        end_quiz = "yes"
         break
+
+    # once you answer a question add +1 to total questions answered
+    questions_answered += 1
+
+
+    # Rounds end here
+
+    # if user has entered exit code, end game!!
+    if end_quiz == "yes":
+        break
+
+    questions_played += 1
+
+    # Add round result to game history
+    history_feedback = f"Round {questions_played}: {feedback}"
+    quiz_history.append(history_feedback)
+
+    # add guesses used to score list
+    all_scores.append(questions_answered)
+
+    # if users are in infinite mode, increase number of questions by +1
+    if mode == "infinite":
+        num_questions += 1
+
+# Quiz ends here
+
+# Before calculating statistics:
+if questions_played > 0:
+    # History / Statistics area
+
+    # Calculate statistics
+    all_scores.sort()
+    best_score = all_scores[0]
+    worst_score = all_scores[-1]
+    average_score = sum(all_scores) / len(all_scores)
+
+    # Output Game Statistics
+    print("📊📊📊 Game Statistics 📊📊📊")
+    print(f"Best:{best_score} | Worst:{worst_score} | Average:{average_score:.2f} ")
+    print()
+
+    # ask the user if they want to see their game history and output if requested.
+    see_history = yes_no("\nDo you want to see your game history? ")
+    if see_history == "yes":
+        for item in quiz_history:
+            print(item)
+            if see_history == "no":
+                break
+
+    print()
+    print("Thanks for playing.")
+
+
+
+
+
